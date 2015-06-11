436717ce61851f02b3cc77feebd1d8cb52b2ba5e
Roni Mendelsohn <ronim@breezometer.com>
2015-06-11 15:02:08 +0300

    First functional test specced out in comments. Now uses unittest framework

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Julie has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-assert 'Django' in browser.title
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy concert tickets" into a text box
+
+        # When she hits enter the page updates and now the page lists
+        # "1. Buy concert tickets" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item
+        # She enters "Buy a sofa"
+
+        # The page updates again and now shows both items on her list
+
+        # Julie wonders whether the site will remember her list.
+        # Than she sees that the site has generated a unique URL just for her
+        # -- there is some explanatory text to that effect
+
+        # She visits that URL - her list is still there
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
